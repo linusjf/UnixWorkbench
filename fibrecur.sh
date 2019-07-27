f06a56271d5b73a1e3464339f701db3449e50670
fernal73 <linus.fernandes@gmail.com>
2019-07-27 06:24:15 +0530

    Fixed static code analysis error

--- fibrecur.sh
+++ fibrecur.sh
@@ -11,7 +11,7 @@ fibrecur(){
   
 # fibrecur() is called until the value of $g is returned and it is < 2
 # This is called the recursion
-  [ $i -lt 2 ] && echo $i || { f=$(( i - 1));
+  [ "$i" -lt 2 ] && echo "$i" || { f=$(( i - 1));
  f=$(fibrecur $f);g=$(( i - 
 2));g=$(fibrecur $g); g=$(( f + g )); echo $g; }
 }
@@ -22,4 +22,4 @@ fibrecur(){
 exit 1; }
 
 # call fibrecur
-fibrecur $1
+fibrecur "$1"
