a734e2ca5cea3679842e8a9b11fad31da57834c8
fernal73 <linus.fernandes@gmail.com>
2019-10-25 11:26:37 +0530

    More piping examples

--- Parallel/pipe.sh
+++ Parallel/pipe.sh
@@ -11,4 +11,58 @@ echo
 echo -e "cat num1000000 | parallel --pipe -j4 --keep-order --round-robin wc\n"
 cat num1000000 | parallel --pipe -j4 --keep-order --round-robin wc
 echo
+echo -e "cat num1000000 | parallel --pipe -N140000 wc\n"  
+cat num1000000 | parallel --pipe -N140000 wc
+echo
+echo -e "cat num1000000 | parallel --pipe -L75 wc\n"
+cat num1000000 | parallel --keep-order --pipe -L75 wc
+echo
+echo -e "echo /foo, bar/, /baz, qux/, | \
+ parallel --dryrun -kN1 --recend ', ' --pipe echo JOB{#}\;cat\;echo END\n"
+echo /foo, bar/, /baz, qux/, | \
+ parallel --dryrun -kN1 --recend ', ' --pipe echo JOB{#}\;cat\;echo END
+echo
+echo -e "echo -n '/foo, bar/, /baz, qux/, ' | \
+ parallel -kN1 --recend ', ' --pipe echo JOB{#}\;cat\;echo END\n"
+echo -n '/foo, bar/, /baz, qux/, ' | \
+ parallel -kN1 --recend ', ' --pipe echo JOB{#}\;cat\;echo END
+echo
+echo -e "echo -n '/foo, bar/, /baz, qux/, ' | \
+ parallel -kN1 --recstart / --pipe echo JOB{#}\;cat\;echo END\n"
+echo -n '/foo, bar/, /baz, qux/, ' | \
+ parallel -kN1 --recstart / --pipe echo JOB{#}\;cat\;echo END
+echo
+echo -e "echo /foo, bar/, /baz, qux/, | \
+ parallel -kN1 --recend ', ' --recstart / --pipe \
+ echo JOB{#}\;cat\;echo END\n"
+echo -n '/foo, bar/, /baz, qux/, ' | \
+ parallel -kN1 --recend ', ' --recstart / --pipe \
+ echo JOB{#}\;cat\;echo END
+echo
+echo -e "echo -n 'foo,bar,_baz,__qux ' | \
+ parallel -kN1 --regexp --recend ,_* --pipe \
+ echo JOB{#}\;cat\;echo END\n"
+echo -n 'foo,bar,_baz,__qux ' | \
+ parallel -kN1 --regexp --recend ,_* --pipe \
+ echo JOB{#}\;cat\;echo END
+echo
+echo -e "echo -n 'foo,bar,_baz,__qux' | \
+ parallel -kN1 --rrs --regexp --recend ,_* --pipe \
+ echo JOB{#}\;cat\;echo END\n"
+echo -n 'foo,bar,_baz,__qux' | \
+ parallel -kN1 --rrs --regexp --recend ,_* --pipe \
+ echo JOB{#}\;cat\;echo END
+echo
+echo -e "echo -n 'foo,bar,_baz,__qux' | \
+ parallel -kN1 --rrs --regexp --recend ,_* --pipe \
+ echo JOB{#}';'cat';'echo END\n"
+echo -n 'foo,bar,_baz,__qux' | \
+ parallel -kN1 --rrs --regexp --recend ,_* --pipe \
+ echo JOB{#}';'cat';'echo END
+echo
+echo -e "cat num_%header | \
+ parallel --header '(%.*\n)*' --pipe -N3 echo JOB{#}\;cat\n"
+cat num_%header | \
+ parallel --header '(%.*\n)*' --pipe -N3 echo JOB{#}\;cat
+echo
 exit 0
