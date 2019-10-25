04ec6cb1c660ab028cd8870ca92562706c05b2ae
fernal73 <linus.fernandes@gmail.com>
2019-10-25 19:42:07 +0530

    mutexing

--- Parallel/mutex.sh
+++ Parallel/mutex.sh
@@ -1,16 +1,17 @@
 #!/usr/bin/env bash
-sem 'sleep 1; echo The first finished' && \
+sem 'sleep 10; echo The first finished' && \
 echo The first is now running in the background && \
-sem 'sleep 1; echo The second finished' && \
+sem 'sleep 10; echo The second finished' && \
 echo The second is now running in the background
 sem --wait
 
-sem --fg 'sleep 1; echo The first finished' && \
+sem --fg 'sleep 10; echo The first finished' && \
 echo The first finished running in the foreground && \
-sem --fg 'sleep 1; echo The second finished' && \
+sem --fg 'sleep 10; echo The second finished' && \
 echo The second finished running in the foreground
 sem --wait
 
 sem --id my_id -u 'echo First started; sleep 10; echo First done' && \
-sem --id my_id -u 'echo Second started; echo Second done' 
+sem --id my_id -u 'echo Second started; sleep 10; echo Second done' \ 
+sem --id my_id -u 'echo all done;' 
 sem --wait
