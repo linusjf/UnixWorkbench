392e3c046f18a902a64c90327d0111b4001be4b8
fernal73 <linus.fernandes@gmail.com>
2019-10-25 15:27:47 +0530

    Added more pipe commands

--- Parallel/pipe.sh
+++ Parallel/pipe.sh
@@ -73,4 +73,36 @@ echo
 echo -e "cat num_%header | parallel --header 2 --pipe -N3 echo JOB{#}\;cat\n"
 cat num_%header | parallel --header 2 --pipe -N3 echo JOB{#}\;cat
 echo
+echo -e "cat fixedlen | parallel --pipe --header .{4} --block 3 --recend '' \
+ 'echo start; cat; echo'\n"
+cat fixedlen | parallel --pipe --header .{4} --block 3 --recend '' \
+ 'echo start; cat; echo'
+echo
+echo -e "cat num1000000 | parallel --pipe --cat wc {}\n"
+cat num1000000 | parallel --pipe --cat wc {}
+echo
+echo -e "cat num1000000 | parallel --pipe --cat --gnu time wc {}\n"
+cat num1000000 | parallel --pipe --cat --gnu time wc {}
+echo
+echo -e "cat num1000000 | parallel --pipe --fifo --gnu time wc {}\n"
+cat num1000000 | parallel --pipe --fifo --gnu time wc {}
+echo
+echo -e "parallel --pipepart -a num1000000 --block 3m wc\n"
+parallel --pipepart -a num1000000 --block 3m wc
+echo
+echo -e "parallel --pipepart -a num1000000 --block -3 -j5 -k wc\n"
+parallel --pipepart -a num1000000 --block -3 -j5 -k wc
+echo
+echo -e "seq 1000 | parallel --dryrun --pipe --tee -v wc {} ::: -w -l -c\n"
+seq 1000 | parallel --dryrun --pipe --tee -v wc {} ::: -w -l -c
+echo
+echo -e "seq 1000 | parallel --pipe --tee -v wc {} ::: -w -l -c\n"
+seq 1000 | parallel --pipe --tee -v wc {} ::: -w -l -c
+echo
+echo -e "seq 30 | parallel --dryrun -v --pipe --tee --tag grep {} ::: 4 5 6\n"
+seq 30 | parallel --dryrun -v --pipe --tee --tag grep {} ::: 4 5 6
+echo
+echo -e "seq 30 | parallel -v --pipe --tee --tag grep {} ::: 4 5 6\n"
+seq 30 | parallel -v --pipe --tee --tag grep {} ::: 4 5 6
+echo
 exit 0
