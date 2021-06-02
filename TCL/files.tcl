61e3ec09cdbac7c6647a0f969b9fc52d8922d4d8
fernal73 <linus.fernandes@gmail.com>
2021-06-02 15:16:52 +0530

    Add more file command samples

--- TCL/files.tcl
+++ TCL/files.tcl
@@ -18,3 +18,22 @@ set days {Monday Tuesday Wednesday Thursday Friday Saturday Sunday}
 
 puts $fp $days
 close $fp
+
+set fp [open languages r]
+set data [read $fp]
+puts -nonewline $data
+close $fp
+
+set fp [open languages]
+while {![eof $fp]} {
+    puts [gets $fp]
+}
+close $fp
+
+set fp [open newfile w]
+
+puts $fp "this is new file"
+flush $fp
+file copy newfile newfile2
+file delete newfile
+close $fp
