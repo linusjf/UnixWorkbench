b677ca27b2c0abd043ab8ee97c18ea9728f5d17d
Linus Fernandes <linus.fernandes@gmail.com>
2023-01-13 14:47:34 +0530

    feat: add more string examples

--- Programs/strings.sh
+++ Programs/strings.sh
@@ -1,8 +1,19 @@
-#!/bin/bash
+#!/usr/bin/env bash
 # File strings.sh
 first='Hello, '
 second='World!'
-echo $first
-echo $second
-echo $first $second
-
+echo "$first"
+echo "$second"
+echo "$first" "$second"
+distro="Ubuntu"
+echo "${#distro}"
+str1="hand"
+str2="book"
+str3="$str1""$str2"
+echo "$str3"
+str="Bash is Cool"
+word="Cool"
+echo "$(expr index "$str" "$word")"
+foss="Fedora is a free operating system"
+echo "${foss:0:6}"
+echo "${foss:12}"
