5225bfbc494f9cdb7166f3d812a0bbaee8a929ff
Linus Fernandes <linus.fernandes@gmail.com>
2023-01-26 21:45:32 +0530

    feat: add until example

--- Programs/ifloop.sh
+++ Programs/ifloop.sh
@@ -1,9 +1,7 @@
 #!/usr/bin/env bash
 # File: ifloop.sh
-for number in {1..10}
-do
-  if [[ $number -lt 3 ]] || [[ $number -gt 8 ]]
-  then
-    echo $number
+for number in {1..10}; do
+  if [[ $number -lt 3 ]] || [[ $number -gt 8 ]]; then
+    echo "$number"
   fi
 done
