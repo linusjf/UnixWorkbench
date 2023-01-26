923f51a820c1a2acb9a24cdba3ec91fdcf75d428
Linus Fernandes <linus.fernandes@gmail.com>
2023-01-26 21:48:22 +0530

    feat: add until example

--- Programs/isiteven.sh
+++ Programs/isiteven.sh
@@ -1,13 +1,12 @@
 #!/usr/bin/env bash
 # File isiteven.sh
 function isItEven() {
-  if [[ "$1" =~ ^[0-9]+$ ]]
-  then
-    if [[ "$1"%2 -eq 0 ]]
-    then
+  if [[ "$1" =~ ^[0-9]+$ ]]; then
+    if [[ $(($1 % 2)) -eq 0 ]]; then
       echo 1
       return 1
-    else [[ "$1"%2 -eq 1 ]]
+    else
+      [[ $(($1 % 2)) -eq 1 ]]
       echo 0
       return 0
     fi
@@ -17,4 +16,3 @@ function isItEven() {
 }
 
 isItEven "$@"
-
