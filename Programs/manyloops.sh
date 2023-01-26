2544b648f243ec6aeafd6ffbfbf564461fa9f648
Linus Fernandes <linus.fernandes@gmail.com>
2023-01-26 22:08:58 +0530

    chore: fix shellcheck violstions

--- Programs/manyloops.sh
+++ Programs/manyloops.sh
@@ -1,20 +1,17 @@
 #!/usr/bin/env bash
 echo "Explicit list:"
-for picture in img001.jpg img002.jpg img451.jpg
-do
+for picture in img001.jpg img002.jpg img451.jpg; do
   echo "picture is equal to $picture"
 done
 echo ""
 echo "Array:"
 stooges=(curly larry moe)
-for stooge in ${stooges[*]}
-do
-  echo "Current stooge: $stooge"
+for stooge in "${stooges[@]}"; do
+  echo "Current stooge: ${stooge}"
 done
 echo ""
 echo "Command substitution:"
-for code in *.sh
-do
+for code in *.sh; do
   [[ -e "$code" ]] || break
   echo "$code"" is a bash script"
 done
